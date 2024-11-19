41ef588275ef41ebcc3b1172827ae93d6b42ef26
bakend666 <2334370017@qq.com>
2024-11-19 11:07:14 +0800

    Changes to be committed: 	modified:   App/Src/RC_task.c

--- App/Src/RC_task.c
+++ App/Src/RC_task.c
@@ -92,7 +92,7 @@ void RC_init(uint8_t *rx1_buf, uint8_t *rx2_buf, uint16_t dma_buf_num)
 
 }
 /* 获取遥控器数据指针 */
-const RC_ctrl_t *get_remote_control_point(void)
+const RC_ctrl_t*  get_remote_control_point(void)
 {
     return &rc_ctrl;
 }
